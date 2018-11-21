9740ea3862cc6de53351f22991f4c6db42f3194c
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-11-21 20:57:21 +0900

    Curry price and order amount should be over 0

---
+++ curry_order/curry_order/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from .models import Curry, Order
+
+
+class LearningTest(TestCase):
+    def test_assert_equals(self):
+        a = 1
+        self.assertIs(a, 1)
+
+class ModelCurryTest(TestCase):
+    def test_curry_price(self):
+        curry = Curry(price = 0)
+        self.assertIs(curry.is_over_0(), False)
+
+
+class ModelOrderTest(TestCase):
+    def test_order_amount(self):
+        order = Order(amount = 0)
+        self.assertIs(order.has_ordered(), False)
+
