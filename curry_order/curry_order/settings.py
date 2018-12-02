2450eb8ab9d0b31ea3ec56e2415f6b390ade5005
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-12-02 20:10:21 +0900

    use environ variable for SECRET_KEY for security.

--- curry_order/curry_order/settings.py
+++ curry_order/curry_order/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^^25+n!=horgi1-f7_e7o5iw875vv4l&b*^s+wiid^s&wl_j1*'
+SECRET_KEY = os.environ['SECRET_KEY']  # '^^25+n!=horgi1-f7_e7o5iw875vv4l&b*^s+wiid^s&wl_j1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
